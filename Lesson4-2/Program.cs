841bb34f55ce616afe566f44b37fe7697c3c1e99
Ruslan <karuslan@inbox.ru>
2022-02-10 00:03:04 +0300

    Задание 2

--- Lesson4-2/Program.cs
+++ Lesson4-2/Program.cs
@@ -1,1 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+static int SummaChislvTexte (string strokaChisla)
+{ 
+    string sumChislaText = null;
+    int sumChislo = 0;
+    for (int i = 0; i < strokaChisla.Length; i++)
+    {
+
+    if (strokaChisla[i] == ' ')
+        {
+        sumChislo += Convert.ToInt32(sumChislaText);
+        sumChislaText = null;
+    }
+    
+    else
+    {
+        sumChislaText += strokaChisla[i];  
+
+    }
+    if(i == strokaChisla.Length - 1)
+    {
+        sumChislo += Convert.ToInt32(sumChislaText);
+    }
+        
+    }
+return sumChislo;
+    
+}
+Console.WriteLine("Введите числа с пробелами");
+int itog = SummaChislvTexte(Console.ReadLine());
+Console.WriteLine(itog);
+Console.ReadLine();
+
+
+
