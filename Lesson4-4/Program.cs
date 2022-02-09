63d1d333568b6ee9be1181ce49c8cb5b7352a20c
Ruslan <karuslan@inbox.ru>
2022-02-09 11:56:58 +0300

    Задание 1

---
+++ Lesson4-4/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+uint number1 = 0;
+uint number2 = 1;
+
+for (int i = 0; i < 4; i++)
+{
+    uint fibonachhi = number1 + number2;
+    Console.WriteLine(fibonachhi);
+    number2 = fibonachhi;
+    fibonachhi += fibonachhi;
+    number1 = fibonachhi;
+    Console.WriteLine(fibonachhi);
+}
+Console.ReadLine();
+
