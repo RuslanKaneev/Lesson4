7b4cafee1f57182e608a26c9ca0c8877a87b5b3f
Ruslan <karuslan@inbox.ru>
2022-02-10 03:15:24 +0300

    Задание 3

---
+++ Lesson4-3/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите номер месяца");
+int nomerMonth = Convert.ToInt32(Console.ReadLine());
+
+if (nomerMonth > 0 && nomerMonth < 13)
+{
+    Season seasonOfTheYear = VremenaGoda(nomerMonth);
+    string russianSeasonOfTheYear = RussianVremenaGoda(seasonOfTheYear);
+    Console.WriteLine(russianSeasonOfTheYear);
+}
+else
+{
+    Console.WriteLine("Ошибка: введите число от 1 до 12");
+}
+
+Console.ReadLine();
+
+static Season VremenaGoda (int nomerMonth)
+{ 
+    switch (nomerMonth)
+    {
+        case 1:
+        case 2:
+        case 12:
+            return Season.Winter;
+
+        case 3:
+        case 4:
+        case 5:
+            return Season.Spring;
+            
+        case 6:
+        case 7:
+        case 8:
+            return Season.Summer;
+           
+        default:
+            return Season.Autumn;
+            
+    }
+}
+
+static string RussianVremenaGoda(Season seasonOfTheYear)
+{ 
+ 
+    switch (seasonOfTheYear)
+    {
+        case Season.Winter:
+            return "Зима";
+
+        case Season.Spring:
+            return "Весна";
+
+        case Season.Summer:
+            return "Лето";
+
+        case Season.Autumn:
+            return "Осень";
+           
+        default: 
+            break;
+    }
+    return "Ошибка";
+}
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
+}
+
+
+
+
